4a65b1a5c11c945bcaed54e5d392582864be3f9d
zheronprecision <cagrijunk@gmail.com>
2026-08-15 00:23:01 +0300

    Apply official ZHERON identity across site

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./style.css";
+import "./brand.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
